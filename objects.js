0aae73d9037d5d888230a668cc61cc6dd04cb0e0
Elizabeth Allene Norton <norton.allene@gmail.com>
2020-04-30 20:26:05 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
-var playlist = {
+var playlist = new Object({
   MichaelJ : 'Thriller',
   BritneySpears : 'Stronger',
   Prince : 'Purple Rain'
-  };
+})
 function updatePlaylist(playlist, artist, song){
   return Object.assign({},playlist,{[artist]: song})
 }
